e20151da6cedc9419bcc31c70d6bf05cdf0a5ccf
Alex Suraci <asuraci@pivotallabs.com>
2014-07-30 12:40:13 -0700

    deps

--- Godeps/_workspace/src/github.com/cloudfoundry-incubator/runtime-schema/bbs/lrp_bbs/lrp_getters_test.go
+++ Godeps/_workspace/src/github.com/cloudfoundry-incubator/runtime-schema/bbs/lrp_bbs/lrp_getters_test.go
@@ -1,6 +1,7 @@
 package lrp_bbs_test
 
 import (
+	"github.com/cloudfoundry-incubator/runtime-schema/bbs/lrp_bbs"
 	"github.com/cloudfoundry-incubator/runtime-schema/models"
 
 	. "github.com/onsi/ginkgo"
@@ -12,9 +13,44 @@ var _ = Describe("LrpGetters", func() {
 	var desiredLrp1, desiredLrp2, desiredLrp3 models.DesiredLRP
 
 	BeforeEach(func() {
-		desiredLrp1 = models.DesiredLRP{ProcessGuid: "guidA", Stack: "stack", Source: "http://jelly.donut"}
-		desiredLrp2 = models.DesiredLRP{ProcessGuid: "guidB", Stack: "stack", Source: "http://jelly.donut"}
-		desiredLrp3 = models.DesiredLRP{ProcessGuid: "guidC", Stack: "stack", Source: "http://jelly.donut"}
+		desiredLrp1 = models.DesiredLRP{
+			ProcessGuid: "guidA",
+			Stack:       "stack",
+			Actions: []models.ExecutorAction{
+				{
+					Action: models.DownloadAction{
+						From: "http://example.com",
+						To:   "/tmp/internet",
+					},
+				},
+			},
+		}
+
+		desiredLrp2 = models.DesiredLRP{
+			ProcessGuid: "guidB",
+			Stack:       "stack",
+			Actions: []models.ExecutorAction{
+				{
+					Action: models.DownloadAction{
+						From: "http://example.com",
+						To:   "/tmp/internet",
+					},
+				},
+			},
+		}
+
+		desiredLrp3 = models.DesiredLRP{
+			ProcessGuid: "guidC",
+			Stack:       "stack",
+			Actions: []models.ExecutorAction{
+				{
+					Action: models.DownloadAction{
+						From: "http://example.com",
+						To:   "/tmp/internet",
+					},
+				},
+			},
+		}
 
 		lrp1 = models.ActualLRP{
 			ProcessGuid:  "guidA",
@@ -24,6 +60,7 @@ var _ = Describe("LrpGetters", func() {
 			Since:        timeProvider.Time().UnixNano(),
 			ExecutorID:   "executor-id",
 		}
+
 		lrp2 = models.ActualLRP{
 			ProcessGuid:  "guidA",
 			Index:        2,
@@ -32,6 +69,7 @@ var _ = Describe("LrpGetters", func() {
 			Since:        timeProvider.Time().UnixNano(),
 			ExecutorID:   "executor-id",
 		}
+
 		lrp3 = models.ActualLRP{
 			ProcessGuid:  "guidB",
 			Index:        2,
@@ -62,6 +100,38 @@ var _ = Describe("LrpGetters", func() {
 			Ω(all).Should(ContainElement(desiredLrp1))
 			Ω(all).Should(ContainElement(desiredLrp2))
 			Ω(all).Should(ContainElement(desiredLrp3))
+		})
+	})
+
+	Describe("GetAllDesiredLRPsByDomain", func() {
+		BeforeEach(func() {
+			desiredLrp1.Domain = "domain-1"
+			desiredLrp2.Domain = "domain-1"
+			desiredLrp3.Domain = "domain-2"
+
+			err := bbs.DesireLRP(desiredLrp1)
+			Ω(err).ShouldNot(HaveOccurred())
+
+			err = bbs.DesireLRP(desiredLrp2)
+			Ω(err).ShouldNot(HaveOccurred())
+
+			err = bbs.DesireLRP(desiredLrp3)
+			Ω(err).ShouldNot(HaveOccurred())
+		})
+
+		It("returns all desired long running processes for the given domain", func() {
+			byDomain, err := bbs.GetAllDesiredLRPsByDomain("domain-1")
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(byDomain).Should(ConsistOf([]models.DesiredLRP{desiredLrp1, desiredLrp2}))
+
+			byDomain, err = bbs.GetAllDesiredLRPsByDomain("domain-2")
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(byDomain).Should(ConsistOf([]models.DesiredLRP{desiredLrp3}))
+		})
+
+		It("blows up with an empty string domain", func() {
+			_, err := bbs.GetAllDesiredLRPsByDomain("")
+			Ω(err).Should(Equal(lrp_bbs.ErrNoDomain))
 		})
 	})
 
