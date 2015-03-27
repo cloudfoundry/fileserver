ea2496b5ccc4a6e10054d9256cae634430678651
Amit Gupta <agupta@pivotal.io>
2015-03-27 11:57:26 -0700

    Eliminate dot imports [#91296030]
    Signed-off-by: Atul Kshirsagar <atul.kshirsagar@gmail.com>

--- ccclient/fake_ccclient/fakes_test.go
+++ ccclient/fake_ccclient/fakes_test.go
@@ -2,17 +2,17 @@ package fake_ccclient_test
 
 import (
 	"github.com/cloudfoundry-incubator/file-server/ccclient"
-	. "github.com/cloudfoundry-incubator/file-server/ccclient/fake_ccclient"
+	"github.com/cloudfoundry-incubator/file-server/ccclient/fake_ccclient"
 
 	. "github.com/onsi/ginkgo"
 )
 
 var _ = Describe("Fakes", func() {
 	It("is an Uploader", func() {
-		var _ ccclient.Uploader = &FakeUploader{}
+		var _ ccclient.Uploader = &fake_ccclient.FakeUploader{}
 	})
 
 	It("is a Poller", func() {
-		var _ ccclient.Poller = &FakePoller{}
+		var _ ccclient.Poller = &fake_ccclient.FakePoller{}
 	})
 })
