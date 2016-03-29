d700321bab98ee25119c4655ff335ed91d813bf0
Connor Braa <cbraa@pivotal.io>
2016-03-29 09:44:57 -0700

    remove useless test dependency on fake_cc
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

--- cmd/file-server/main_suite_test.go
+++ cmd/file-server/main_suite_test.go
@@ -1,23 +1,16 @@
 package main_test
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/cloudfoundry-incubator/consuladapter/consulrunner"
-	"github.com/cloudfoundry-incubator/inigo/fake_cc"
 	. "github.com/onsi/ginkgo"
 	"github.com/onsi/ginkgo/config"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
-	"github.com/tedsuo/ifrit"
 
 	"testing"
 )
 
 var fileServerBinary string
-var fakeCC *fake_cc.FakeCC
-var fakeCCProcess ifrit.Process
 var consulRunner *consulrunner.ClusterRunner
 
 func TestFileServer(t *testing.T) {
@@ -30,9 +23,6 @@ var _ = SynchronizedBeforeSuite(func() []byte {
 	Expect(err).NotTo(HaveOccurred())
 	return []byte(fileServerPath)
 }, func(fileServerPath []byte) {
-	fakeCCAddress := fmt.Sprintf("127.0.0.1:%d", 6767+GinkgoParallelNode())
-	fakeCC = fake_cc.New(fakeCCAddress)
-
 	fileServerBinary = string(fileServerPath)
 
 	consulRunner = consulrunner.NewClusterRunner(
@@ -53,10 +43,4 @@ var _ = SynchronizedAfterSuite(func() {
 
 var _ = BeforeEach(func() {
 	consulRunner.Reset()
-	fakeCCProcess = ifrit.Envoke(fakeCC)
-})
-
-var _ = AfterEach(func() {
-	fakeCCProcess.Signal(os.Kill)
-	Eventually(fakeCCProcess.Wait()).Should(Receive(BeNil()))
 })
